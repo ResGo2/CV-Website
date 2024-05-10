49767da6844297345548e577e21f6993e624350a
ResGo2 <102379645+ResGo2@users.noreply.github.com>
2024-05-10 18:24:08 +0200

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,14 @@
+
+var tablinks = document.getElementsByClassName("tablinks");
+var tabcontents = document.getElementsByClassName("tabcontent");
+
+function opentab(tabname) {
+    for (tablink of tablinks){
+        tablink.classList.remove("activelink");
+    }
+    for (tabcontent of tabcontents){
+        tabcontent.classList.remove("activetab");
+    }
+    event.currentTarget.classList.add("activelink");
+    document.getElementById(tabname).classList.add("activetab");
+}
